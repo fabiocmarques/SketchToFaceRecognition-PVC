0416892231a59ac3aa98b56157f500d2ee6e6d7f
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2018-06-22 01:10:05 -0300

    Adding findPhoto

--- SketchToFace-FG.py
+++ SketchToFace-FG.py
@@ -37,9 +37,19 @@ def generateSiftDescriptors(imgList):
                 _, desc = sift.compute(img, [kp])
                 descList[-1].append(desc[0])
 
-        print(len(descList[-1]))
-        exit(0)
-    return descList
+    return np.asarray(descList)
+
+def findPhoto(sketchDesc, photosDescs):
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    search_params = dict(checks=50)   # or pass empty dictionary
+
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    
+    for photoDesc in photosDescs:
+        matches = flann.knnMatch(sketchDesc, photoDesc, 2)
+        print(sum([m[0].distance + m[1].distance for m in matches]))
 
 def main():
     print("start")
@@ -48,6 +58,8 @@ def main():
 
     sketchesDescs = generateSiftDescriptors(sketches)
     photosDescs = generateSiftDescriptors(photos)
+
+    findPhoto(sketchesDescs[5], photosDescs)
 
 if __name__ == "__main__":
     main()
